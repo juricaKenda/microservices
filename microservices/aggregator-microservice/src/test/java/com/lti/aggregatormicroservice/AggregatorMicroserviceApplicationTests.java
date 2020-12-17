619cbafe2b0db02c77a543d5b20950e3ae4a12bf
Jurica Kenda <jurica.kenda@gmail.com>
2020-12-17 22:11:42 +0100

    init

---
+++ microservices/aggregator-microservice/src/test/java/com/lti/aggregatormicroservice/AggregatorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.aggregatormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ microservices/humidity-microservice/src/main/java/com/lti/humiditymicroservice/HumidityMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lti.humiditymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumidityMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumidityMicroserviceApplication.class, args);
+	}
+
+}
