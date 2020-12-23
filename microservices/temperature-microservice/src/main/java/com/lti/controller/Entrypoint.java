0493aef03e87a295c963ed12cf92d58d2d29fa3d
jezicKarlo <karlo.jezic@gmail.com>
2020-12-23 17:47:35 +0100

    start of implementation

--- microservices/temperature-microservice/src/main/java/com/lti/controller/Entrypoint.java
+++ microservices/temperature-microservice/src/main/java/com/lti/controller/Entrypoint.java
@@ -1,7 +1,18 @@
 package com.lti.controller;
 
+import com.lti.response.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/temperatures")
 public class Entrypoint {
+
+
+    @GetMapping
+    public ResponseEntity<Response> getTemperature() {
+
+    }
 }
---
+++ microservices/temperature-microservice/src/main/java/com/lti/response/Response.java
@@ -0,0 +1,11 @@
+package com.lti.response;
+
+import lombok.Data;
+
+@Data
+public class Response {
+
+    private String temperature;
+    private String message;
+
+}
